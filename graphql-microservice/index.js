235d965d67ce5983dbff29b6a7f30b9282320b3b
vani <vania.patzi@ucb.edu.bo>
2025-05-03 01:29:38 -0400

    creando login

--- graphql-microservice/index.js
+++ graphql-microservice/index.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const cors = require("cors");
 const { request, gql } = require("graphql-request");
+const axios = require("axios");
+
 
 const app = express();
 app.use(cors());
 
 const PORT = 4002;
-const GRAPHQL_ENDPOINT = "http://localhost:4001/graphql";
+const LOGIN_URL = "http://localhost:4001/api/auth/login";
+const GRAPHQL_ENDPOINT = "http://localhost:4000/graphql";
+
+// Paso 1: Login y obtener token JWT
+const loginAndGetToken = async () => {
+  const response = await axios.post(LOGIN_URL, {
+    username: "admin",    // tu usuario real
+    password: "admin123"  // tu contraseña real
+  });
+  return response.data.token;
+};
 
 const FILMS_QUERY = gql`
   query {
@@ -16,6 +28,20 @@ const FILMS_QUERY = gql`
     }
   }
 `;
+
+const getFilms = async () => {
+    const token = await loginAndGetToken();
+  
+    const response = await request({
+      url: GRAPHQL_URL,
+      document: FILMS_QUERY,
+      requestHeaders: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  
+    return response.films;
+  };
 
 app.get("/film-stats", async (req, res) => {
   try {
